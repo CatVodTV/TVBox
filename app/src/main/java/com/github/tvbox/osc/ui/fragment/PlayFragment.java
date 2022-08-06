00d019f761f04b157dc1313d731e11722660a71e
CatVodTV <106614660+CatVodTV@users.noreply.github.com>
2022-08-06 21:45:27 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -355,7 +355,7 @@ public class PlayFragment extends BaseLazyFragment {
                 mVodPlayerCfg.put("sp", 1.0f);
             }
             if (!mVodPlayerCfg.has("st")) {
-                mVodPlayerCfg.put("st", 120);
+                mVodPlayerCfg.put("st", 80);
             }
             if (!mVodPlayerCfg.has("et")) {
                 mVodPlayerCfg.put("et", 120);
